4e584cdb828ae8fdba732f894e8086494b0611e4
Aphillip22 <amber.rain.phillips@gmail.com>
2021-11-13 11:47:32 -0700

    add uri

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 // import Sequelize constructor from library
 const Sequelize = require('sequelize');
-
+const uri = 'heroku_033f2d7fd825089';
+const username = 'b14991b282fc47';
+const pw = '7e3e65e5';
+const host = 'http://us-cdbr-east-02.cleardb.com/'
 require('dotenv').config();
 
 // create connection to database
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+  : new Sequelize(uri, username, pw, {
+      host: host,
       dialect: 'mysql',
       port: 3306
     });
