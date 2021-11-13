24b9cba3a2ffb40e10f803cee956011236e5faac
Aphillip22 <amber.rain.phillips@gmail.com>
2021-11-13 12:22:18 -0700

    update config

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,16 @@
 // import Sequelize constructor from library
 const Sequelize = require('sequelize');
-const uri = 'heroku_033f2d7fd825089';
-const username = 'b14991b282fc47';
-const pw = '99d8bdf0a7c1029';
-const host = 'us-cdbr-east-02.cleardb.com'
+
 require('dotenv').config();
 
 // create connection to database
-// const sequelize = !process.env.JAWSDB_URL
-  // ? new Sequelize('mysql://b14991b282fc47:99d8bdf0a7c1029@us-cdbr-east-04.cleardb.com/heroku_033f2d7fd825089?reconnect=true')
-  // : new Sequelize(uri, username, pw, {
-      // host: host,
-      // dialect: 'mysql',
-      // port: 3306
-    // });
+const sequelize = process.env.CLEARDB_DATABASE_URL
+  ? new Sequelize(process.env.CLEARDB_DATABASE_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: host,
+      dialect: 'mysql',
+      port: 3306
+    });
 
-    const sequelize = new Sequelize('mysql://b14991b282fc47:99d8bdf0a7c1029@us-cdbr-east-04.cleardb.com/heroku_033f2d7fd825089?reconnect=true');
-    
+
 module.exports = sequelize;
